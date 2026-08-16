f193b647ce058bdaaa02a55a6bae6197c643f2e9
harishraj-021 <harishrajsb2007@gmail.com>
2026-08-16 10:39:14 +0530

    Delete frontend/src/components/layout/Input.tsx

--- frontend/src/components/layout/Input.tsx
+++
@@ -1,6 +0,0 @@
-import type { InputHTMLAttributes } from 'react'
-import { cn } from '@/lib/utils'
-
-export function Input({ className, ...props }: InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={cn('h-11 w-full rounded-xl border border-white/8 bg-[#0c0f15] px-3.5 text-sm text-white outline-none transition placeholder:text-white/25 focus:border-red-400/40 focus:ring-2 focus:ring-red-400/10', className)} {...props} />
-}
